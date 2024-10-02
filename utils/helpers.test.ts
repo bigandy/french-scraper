2c896054a2b59b5e0117862ffc1a280335684187
Andrew Hudson <ahudson@gmail.com>
2024-10-02 14:09:16 +0200

    adds vitest

---
+++ utils/helpers.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+
+import { capitalizeFirstLetter } from "./helpers";
+
+test("capitalize first letter", () => {
+  expect(capitalizeFirstLetter("andrew")).toBe("Andrew");
+
+  expect(capitalizeFirstLetter("Andrew")).not.toBe("andrew");
+  expect(capitalizeFirstLetter("andré")).toBe("André");
+});
