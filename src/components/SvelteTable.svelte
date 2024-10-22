b258f87377f93fa8640de7f5f74e836f632a7874
Andrew Hudson <ahudson@gmail.com>
2024-10-22 18:20:57 +0200

    start on creating SvelteTable component

---
+++ src/components/SvelteTable.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  interface Props {
+    rows: any[];
+  }
+  const { rows }: Props = $props();
+  let rowValues = $state(
+    rows.map(([key, value]) => {
+      console.log({ key, value });
+
+      return value;
+    })
+  );
+
+  console.log({ rowValues });
+</script>
+
+<div>
+  <table>
+    <tbody>
+      {#each rows as [key, value]}
+        <tr>
+          <td>{key}</td>
+          <td>
+            {value}
+            <input {value} />
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
