16c1cca64b950d7bebd0480f4756b1bbf62805a4
Andrew Hudson <ahudson@gmail.com>
2024-10-22 11:26:32 +0200

    remove accents in urls as Astro not handling in dynamic routes

--- src/pages/utils.ts
+++ src/pages/utils.ts
@@ -4,15 +4,17 @@ export const getVerbs = async () => {
 	
 	const allVerbs = await getCollection('verbs')
 
+    // console.log({allVerbs})
+
     // Just get the data 
     const allData = allVerbs.map((v: any) => v.data);
 
-    let sorted = allData.sort((a,b) => a.name.localeCompare(b.name));
+    // const sorted = allData.sort((a,b) => a.name.localeCompare(b.name));
 
-    // sorted = sorted.slice(0, 10);
 
-	
-    console.log({sorted})
 
-	return sorted
+    return allData.map((verb: any) => ({...verb, slug: getRidofAccentsInUrls(verb.name)}));
+	// return sorted
 }
+
+export const getRidofAccentsInUrls = (url: string) => url.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
