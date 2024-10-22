2d558416c69efc4c1e4c8703b03036e1651afcb8
Andrew Hudson <ahudson@gmail.com>
2024-10-22 14:48:14 +0200

    show more info about Verbs

--- src/pages/utils.ts
+++ src/pages/utils.ts
@@ -1,20 +1,21 @@
 import { getCollection } from 'astro:content'
 
 export const getVerbs = async () => {
-	
 	const allVerbs = await getCollection('verbs')
 
-    // console.log({allVerbs})
+	// console.log({allVerbs})
 
-    // Just get the data 
-    const allData = allVerbs.map((v: any) => v.data);
+	// Just get the data
+	const allData = allVerbs.map((v: any) => v.data)
+	const withSlugs = allData.map((verb: any) => ({
+		...verb,
+		slug: getRidofAccentsInUrls(verb.name),
+	}))
+	const sorted = withSlugs.sort((a, b) => a.name.localeCompare(b.name))
 
-    // const sorted = allData.sort((a,b) => a.name.localeCompare(b.name));
-
-
-
-    return allData.map((verb: any) => ({...verb, slug: getRidofAccentsInUrls(verb.name)}));
-	// return sorted
+	// return allData.map((verb: any) => ({...verb, slug: getRidofAccentsInUrls(verb.name)}));
+	return sorted
 }
 
-export const getRidofAccentsInUrls = (url: string) => url.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+export const getRidofAccentsInUrls = (url: string) =>
+	url.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
