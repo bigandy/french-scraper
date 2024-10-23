9c47bd19e5ac70be659db4aa7aa61b195bf39157
Andrew Hudson <ahudson@gmail.com>
2024-10-23 15:24:01 +0200

    resetting all saved tables on page, one table on page

--- src/components/SvelteTable.svelte
+++ src/components/SvelteTable.svelte
@@ -1,31 +1,103 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   interface Props {
     rows: any[];
+    name: string;
   }
-  const { rows }: Props = $props();
-  let rowValues = $state(
-    rows.map(([key, value]) => {
-      console.log({ key, value });
+  const { rows, name }: Props = $props();
+  let loading = $state(true);
 
-      return value;
-    })
-  );
+  const initialRowValues = () => {
+    const thing: any = {};
+    for (const [key] of rows) {
+      thing[key] = "";
+    }
 
-  console.log({ rowValues });
+    return thing;
+  };
+
+  let rowValues = $state(initialRowValues());
+
+  let rowAnswers = $derived.by(() => {
+    const thing: any = {};
+    for (const [key, value] of rows) {
+      thing[key] = value;
+    }
+
+    return thing;
+  });
+
+  const handleInputChange = (e: any, key: string) => {
+    rowValues[key] = e.target.value;
+
+    localStorage.setItem(name, JSON.stringify(rowValues));
+  };
+
+  onMount(() => {
+    const savedData = localStorage.getItem(name);
+
+    if (savedData) {
+      const parsedData = JSON.parse(savedData);
+      rowValues = parsedData;
+      console.log({ parsedData });
+    }
+    loading = false;
+  });
+
+  const handleReset = () => {
+    localStorage.removeItem(name);
+
+    rowValues = initialRowValues();
+  };
 </script>
 
-<div>
+<div class="wrapper">
   <table>
     <tbody>
       {#each rows as [key, value]}
-        <tr>
+        <tr class:correct={rowValues[key] === rowAnswers[key]} class:loading>
           <td>{key}</td>
           <td>
-            {value}
-            <input {value} />
+            <!-- {value} -->
+            <input
+              value={rowValues[key]}
+              oninput={(e) => handleInputChange(e, key)}
+            />
           </td>
         </tr>
       {/each}
     </tbody>
   </table>
+  {#if loading}
+    <div class="loading-message">Loading...</div>
+  {/if}
+
+  <button onclick={handleReset}>Reset Table</button>
 </div>
+
+<style>
+  .correct td {
+    background: green;
+  }
+
+  .loading td {
+    background: black;
+    color: white;
+  }
+
+  .wrapper {
+    position: relative;
+  }
+
+  .loading-message {
+    position: absolute;
+    inset: 0;
+    color: red;
+    z-index: 1;
+    display: grid;
+    place-content: center;
+
+    backdrop-filter: blur(3px);
+  }
+</style>
